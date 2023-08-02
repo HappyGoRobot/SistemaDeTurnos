00f1f9221222732f052a7a7a09ba9b1b15c0f67b
Michel <124099713+michelvillavi@users.noreply.github.com>
2023-08-02 15:46:02 -0300

    BaseDeDatosClinica

--- clinica.sql
+++ clinica.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-07-2023 a las 20:02:29
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 02-08-2023 a las 20:17:23
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,22 +28,43 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `medicos` (
-  `Llarena Sergio` varchar(10) NOT NULL,
-  `Gonzalez Laura` varchar(10) NOT NULL,
-  `Perez  Analia` varchar(10) NOT NULL,
-  `Cortez Estela` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID_Medico` int(11) NOT NULL,
+  `NombreCompleto` varchar(100) DEFAULT NULL,
+  `EspecialidadMedica` varchar(50) DEFAULT NULL,
+  `TelefonoContacto` varchar(15) DEFAULT NULL,
+  `DireccionConsulta` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Estructura de tabla para la tabla `pacientes`
 --
 
-CREATE TABLE `usuario` (
-  `id_usuarios` int(11) NOT NULL,
-  `Contraseña` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `pacientes` (
+  `ID_Paciente` int(11) NOT NULL,
+  `NombreCompleto` varchar(100) DEFAULT NULL,
+  `FechaNacimiento` date DEFAULT NULL,
+  `Genero` varchar(10) DEFAULT NULL,
+  `NumeroSeguroSocial` varchar(20) DEFAULT NULL,
+  `DireccionPaciente` varchar(100) DEFAULT NULL,
+  `TelefonoContacto` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `turnos`
+--
+
+CREATE TABLE `turnos` (
+  `ID_Turno` int(11) NOT NULL,
+  `ID_Medico` int(11) DEFAULT NULL,
+  `ID_Paciente` int(11) DEFAULT NULL,
+  `FechaHoraTurno` datetime DEFAULT NULL,
+  `EstadoTurno` varchar(20) DEFAULT NULL,
+  `Observaciones` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
@@ -53,14 +74,32 @@ CREATE TABLE `usuario` (
 -- Indices de la tabla `medicos`
 --
 ALTER TABLE `medicos`
-  ADD PRIMARY KEY (`Llarena Sergio`,`Gonzalez Laura`,`Perez  Analia`,`Cortez Estela`),
-  ADD UNIQUE KEY `Llarena Sergio` (`Llarena Sergio`);
+  ADD PRIMARY KEY (`ID_Medico`);
 
 --
--- Indices de la tabla `usuario`
+-- Indices de la tabla `pacientes`
 --
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuarios`);
+ALTER TABLE `pacientes`
+  ADD PRIMARY KEY (`ID_Paciente`);
+
+--
+-- Indices de la tabla `turnos`
+--
+ALTER TABLE `turnos`
+  ADD PRIMARY KEY (`ID_Turno`),
+  ADD KEY `ID_Medico` (`ID_Medico`),
+  ADD KEY `ID_Paciente` (`ID_Paciente`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `turnos`
+--
+ALTER TABLE `turnos`
+  ADD CONSTRAINT `turnos_ibfk_1` FOREIGN KEY (`ID_Medico`) REFERENCES `medicos` (`ID_Medico`),
+  ADD CONSTRAINT `turnos_ibfk_2` FOREIGN KEY (`ID_Paciente`) REFERENCES `pacientes` (`ID_Paciente`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
